4a68bdd477704293fc965f79adb113460a64162b
Roberto Zaniboni <zaniboni@gmail.com>
2021-03-02 13:36:24 +0100

    Skeleton project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import css from './App.module.css'
 
 function App() {
+  const newDate = new Date(Date.now())
+  const newDate2 = new Date('2020-07-01')
+
+  const handleClick = (date: string) => {
+    alert(`la data selezionata è: ${date}`)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={css.container} >
+
+      <div className={css.header}>
+        <h2>Calendar compound component</h2>
+      </div>
+      <div className={css.content}>
+
+        <h1>Calendario</h1>
+
+      </div>
+      <div className={css.footer}>
+        <h5>Footer</h5>
+      </div>
     </div>
   );
 }
